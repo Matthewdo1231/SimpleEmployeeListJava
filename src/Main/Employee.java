c300cc6aa2e79b25d0514a912fc7a42cc9976300
MatthewDo <matthewdoarda@gmail.com>
2024-06-18 00:45:46 +0800

    First commit

---
+++ src/Main/Employee.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class Employee {
+    private String employeeName;
+    private int age;
+    private int salary;
+    private String employeeRole;
+    
+    public void setEmployeeName(String name) {
+    	this.employeeName = name;
+    }
+
+   public String getEmployeeName() {
+	   return this.employeeName;
+   }
+   
+} 
---
+++ src/Main/Main.java
@@ -0,0 +1,23 @@
+package Main;
+
+ import java.util.ArrayList;
+ import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	 
+        Employee employee1 = new Employee();
+        employee1.setEmployeeName("Matthewdo");
+        System.out.println(employee1.getEmployeeName());
+		
+		/*class StartProgram{
+		  public void startProgram() {
+			  
+		  }
+	  }*/
+		
+	  
+	}
+
+}
